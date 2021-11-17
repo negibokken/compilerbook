c0cccefe4f685474bfc64be990103e202a1c1edb
bokken <negibokken@gmail.com>
2021-11-17 17:47:38 +0900

    Make pointer aritmetic work

--- 9cc/9cc.h
+++ 9cc/9cc.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Type Type;
 typedef struct Node Node;
 
 //
@@ -82,7 +83,8 @@ typedef enum {
 struct Node {
   NodeKind kind;  // Node kind
   Node* next;     // Next node
-  Token* tok;
+  Type* ty;       // Type, e.g. int or pointer to int
+  Token* tok;     // Representative token
 
   Node* lhs;  // Left-hand side
   Node* rhs;  // Right-hand side
@@ -102,6 +104,23 @@ struct Node {
 };
 
 Function* parse(Token* tok);
+
+// type.c
+
+typedef enum {
+  TY_INT,
+  TY_PTR,
+} TypeKind;
+
+struct Type {
+  TypeKind kind;
+  Type* base;
+};
+
+extern Type* ty_int;
+
+bool is_integer(Type* ty);
+void add_type(Node* node);
 
 //
 // codegen.c
