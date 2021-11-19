4e43529efbba70d5e5fca86a82c07f2ea92e55b0
bokken <negibokken@gmail.com>
2021-11-19 12:55:30 +0900

    Add zero-arity function definition

--- 9cc/9cc.h
+++ 9cc/9cc.h
@@ -54,6 +54,8 @@ struct Obj {
 
 typedef struct Function Function;
 struct Function {
+  Function* next;
+  char* name;
   Node* body;
   Obj* locals;
   int stack_size;
@@ -117,6 +119,7 @@ Function* parse(Token* tok);
 typedef enum {
   TY_INT,
   TY_PTR,
+  TY_FUNC,
 } TypeKind;
 
 struct Type {
@@ -127,12 +130,16 @@ struct Type {
 
   // Declaration
   Token* name;
+
+  // Function type
+  Type* return_ty;
 };
 
 extern Type* ty_int;
 
 bool is_integer(Type* ty);
 Type* pointer_to(Type* base);
+Type* func_type(Type* return_ty);
 void add_type(Node* node);
 
 //
