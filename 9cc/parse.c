861b79df82d9e996d0ab19f35084de1aa5e496b4
bokken <negibokken@gmail.com>
2021-11-20 10:36:21 +0900

    Add [] operator

--- 9cc/parse.c
+++ 9cc/parse.c
@@ -16,6 +16,7 @@ static Node* equality(Token** rest, Token* tok);
 static Node* relational(Token** rest, Token* tok);
 static Node* add(Token** rest, Token* tok);
 static Node* mul(Token** rest, Token* tok);
+static Node* postfix(Token** rest, Token* tok);
 static Node* unary(Token** rest, Token* tok);
 static Node* primary(Token** rest, Token* tok);
 
@@ -430,7 +431,7 @@ static Node* mul(Token** rest, Token* tok) {
 }
 
 // unary = ("+" | "-" | "*" | "&") unary
-//       | primary
+//       | postfix
 static Node* unary(Token** rest, Token* tok) {
   if (equal(tok, "+"))
     return unary(rest, tok->next);
@@ -444,7 +445,21 @@ static Node* unary(Token** rest, Token* tok) {
   if (equal(tok, "*"))
     return new_unary(ND_DEREF, unary(rest, tok->next), tok);
 
-  return primary(rest, tok);
+  return postfix(rest, tok);
+}
+
+// postfix = primary ("[" expr "]")*
+static Node* postfix(Token** rest, Token* tok) {
+  Node* node = primary(&tok, tok);
+
+  while (equal(tok, "[")) {
+    Token* start = tok;
+    Node* idx = expr(&tok, tok->next);
+    tok = skip(tok, "]");
+    node = new_unary(ND_DEREF, new_add(node, idx, start), start);
+  }
+  *rest = tok;
+  return node;
 }
 
 // funcall = ident "(" (assign ("," assign)*)? ")"
