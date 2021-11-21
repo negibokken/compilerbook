6d729d93667e0bbd2019b0f3495cee89e8eaac04
bokken <negibokken@gmail.com>
2021-11-21 17:27:27 +0900

    Add long type

--- 9cc/tokenize.c
+++ 9cc/tokenize.c
@@ -127,8 +127,8 @@ static int read_punct(char* p) {
 
 static bool is_keyword(Token* tok) {
   static char* kw[] = {
-      "return", "if",     "else", "for",    "while",
-      "int",    "sizeof", "char", "struct", "union",
+      "return", "if",   "else",   "for",   "while", "int",
+      "sizeof", "char", "struct", "union", "short", "long",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
