1bdd0525fe6246e31240284795b5b48275f2a57f
bokken <negibokken@gmail.com>
2021-11-21 13:06:05 +0900

    Add union

--- 9cc/tokenize.c
+++ 9cc/tokenize.c
@@ -127,7 +127,8 @@ static int read_punct(char* p) {
 
 static bool is_keyword(Token* tok) {
   static char* kw[] = {
-      "return", "if", "else", "for", "while", "int", "sizeof", "char", "struct",
+      "return", "if",     "else", "for",    "while",
+      "int",    "sizeof", "char", "struct", "union",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
