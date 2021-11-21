3a92461d3371918f6a25691800408bb3c2d1574e
bokken <negibokken@gmail.com>
2021-11-21 13:10:21 +0900

    Add struct assignment

--- 9cc/test/struct.c
+++ 9cc/test/struct.c
@@ -33,9 +33,19 @@ int main() {
   ASSERT(16, ({ struct t {int a; int b;}; struct t y; sizeof(y); }));
   ASSERT(2, ({ struct t {char a[2];}; { struct t {char a[4];}; } struct t y; sizeof(y); }));
   ASSERT(3, ({ struct t {int x;}; int t=1; struct t y; y.x=2; t+y.x; }));
-  
+
   ASSERT(3, ({ struct t {char a;} x; struct t *y = &x; x.a=3; y->a; }));
   ASSERT(3, ({ struct t {char a;} x; struct t *y = &x; y->a=3; x.a; }));
+
+  ASSERT(3, ({ struct {int a,b;} x,y; x.a=3; y=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y; struct t *z=&y; *z=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y, *p=&x, *q=&y; *q=*p; y.a; }));
+  ASSERT(5, ({ struct t {char a, b;} x, y; x.a=5; y=x; y.a; }));
+
+  ASSERT(3, ({ struct {int a,b;} x,y; x.a=3; y=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y; struct t *z=&y; *z=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y, *p=&x, *q=&y; *q=*p; y.a; }));
+  ASSERT(5, ({ struct t {char a, b;} x, y; x.a=5; y=x; y.a; }));
 
   printf("OK\n");
   return 0;
