37d280d3c8c41124064e8bf597740c3deab871d1
bokken <negibokken@gmail.com>
2021-11-22 16:03:05 +0900

    Add &, |, ~, &=, |=, and ^=

--- 9cc/9cc.h
+++ 9cc/9cc.h
@@ -106,6 +106,9 @@ typedef enum {
   ND_DEREF,      // unary *
   ND_NOT,        // !
   ND_BITNOT,     // ~
+  ND_BITAND,     // &
+  ND_BITOR,      // |
+  ND_BITXOR,     // ^
   ND_MOD,        // %
   ND_RETURN,     // "return"
   ND_IF,         // "if"
